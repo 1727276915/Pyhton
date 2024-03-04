64add62cb8118ccdfde3e06b3a472f3171093d13
1727276915 <87937863+1727276915@users.noreply.github.com>
2024-03-04 09:12:45 +0800

    Add files via upload

---
+++ 第一章/class6.py
@@ -0,0 +1,14 @@
+# 常用的格式化符号
+# %c
+# 格式化字符及其ASCII码
+# %s
+# 格式化字符串
+# %d
+# 格式化整数
+# %f
+# 格式化浮点数字，可指定小数点后的精度
+a = "My name is Huang"
+print(a[0])
+
+print("My name is %s and my cat name is %s" % ("Huang", "Tom"))
+
