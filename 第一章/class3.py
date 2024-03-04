134fa345fb6a825229d907081517ff2dac0f405c
1727276915 <87937863+1727276915@users.noreply.github.com>
2024-03-04 09:10:26 +0800

    Delete 第一章 directory

--- 第一章/class3.py
+++
@@ -1,9 +0,0 @@
-# 什么是布尔类型？
-# - 对与错、是与非、0和1（不是那个0和1！！！）、正与反，都是传统意义上的布尔类型，在Python中，统一使用True和False来表示布尔类型
-# - 在布尔类型一般用于表示条件是否成立，成立用True，不成立用False
-# - 布尔类型是数字类型的一个子集
-# - 在Python中，bool函数可以用来测试一个表达式的布尔值结果
-#   bool(0)、bool(-1)、bool(0b10)、bool('')..............
-#   可以使用print(isinstance(a,int))判断a的数据类型是否为int
-a = 15
-print(isinstance(a, int))
